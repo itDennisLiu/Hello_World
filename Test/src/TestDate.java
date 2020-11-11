b44715ade03f8003dd5ce7acc0edfe190d4dc777
dennis liu <xiao_200612@126.com>
2020-11-11 21:51:21 +0800

    TestDate.java

---
+++ Test/src/TestDate.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class TestDate {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+        System.out.println(localDateTime);
+    }
+}
